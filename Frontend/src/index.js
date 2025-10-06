8e68fd97a7cd1aab0ddfe470ddcb7b22362c3725
Aditya Konda <adityakonda04@gmail.com>
2025-10-06 14:31:17 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client.js";
+import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./ContextApi/AuthContext.jsx";
